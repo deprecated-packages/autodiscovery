9fe4652d5163c4a2ba228394b13b858f0c9f42ab
TomasVotruba <tomas.vot@gmail.com>
2020-09-01 23:19:23 +0200

    prepare release

---
+++ tests/NamespaceDetector/NamespaceDetectorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\Autodiscovery\Tests\NamespaceDetector;
+
+use PHPUnit\Framework\TestCase;
+use Symplify\Autodiscovery\NamespaceDetector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+use Symplify\SmartFileSystem\SmartFileSystem;
+
+final class NamespaceDetectorTest extends TestCase
+{
+    public function test(): void
+    {
+        $namespaceDetector = new NamespaceDetector(new SmartFileSystem());
+        $directoryFileInfo = new SmartFileInfo(__DIR__ . '/Source');
+
+        $this->assertSame(
+            'Symplify\Autodiscovery\Tests\NamespaceDetector\Source',
+            $namespaceDetector->detectFromDirectory($directoryFileInfo)
+        );
+    }
+}
